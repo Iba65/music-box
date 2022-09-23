e58c7f40c3c83d649b4ee96f7f3615e49c70a779
Iba65 <front.developer.iba@gmail.com>
2022-09-23 14:04:05 +0200

    servidos con protocolo http service

---
+++ index.js
@@ -0,0 +1,135 @@
+const http = require('http')
+
+let interpretes = [
+  {
+    id: "1",
+    tipo: "Grupo",
+    nombre: "Pink Floyd",
+    imageng: "PinkFloyd/images/PinkFloydFoto.jpg",
+    estilo: "Rock Sinfónico",
+    periodo: "1965-2005",
+    informacion: {
+      Foto: ["images/Concierto.jpg"],
+      Origen: "Londres, Inglaterra, Reino Unido",
+      Generos: ["Rock progresivo", "Art rock", "Música elcectrónica", "Soft rock", "Rock sinfónico"],
+      Actividad: ["1965-1995", "2005", "2014-2015"],
+      OficioalWeb: "www.pinkfloyd.com",
+      Miembros: [
+        "Roger Waters - bajo, voz líder, guitarra, teclados (1965-1985, 2005)",
+        "Rick Wright - teclados, voz (1965-2008)",
+        "Nick Mason - batería, percusión (1965-2014)",
+        "David Gilmour - guitarra, bajo, voz líder, teclados (1968-2014)"
+      ],
+      Description: [
+        "Pink Floyd es una banda de rock británica, fundada en Londres en 1965. Considerada un ícono cultural del siglo XX y una de las bandas más influyentes, exitosas y aclamadas en la historia de la música popular, obtuvo gran popularidad dentro del circuito underground gracias a su música psicodélica y espacial, que con el paso del tiempo evolucionó hacia el rock progresivo y el rock sinfónico adquiriendo la popularidad con la que hoy son recordados. Es conocida por sus canciones de alto contenido filosófico, a veces de crítica política, junto a la experimentación sonora, las innovadoras portadas de sus discos y sus elaborados espectáculos en vivo. Sus ventas sobrepasan los 280 millones de álbumes vendidos en todo el mundo, 97,5 millones de ellos solamente en los Estados Unidos, convirtiéndose en una de las bandas con más ventas en la historia.",
+        "Inicialmente el grupo estaba formado por el batería Nick Mason, el tecladista y vocalista Richard Wright, el bajista y vocalista Roger Waters y el guitarrista y vocalista principal Syd Barrett, quien se convirtió en el primer líder de la banda tras el efímero paso de Bob Klose, que no llegó a aparecer en ningún disco. El errático e impredecible comportamiento de Barrett, causado por el excesivo consumo de drogas (especialmente LSD),  hizo que en diciembre de 1967 su amigo David Gilmour se incorporara al grupo. Unos meses después, Barrett abandonó, y con el cuarteto formado por Mason, Wright, Waters y Gilmour quedó establecida la formación definitiva del grupo.",
+        "La salida de Barrett convirtió a Waters en el principal letrista de la banda, mientras que Gilmour y Wright asumieron el protagonismo en la composición musical. El grupo grabó durante esta época muchos álbumes que se convirtieron en éxitos comerciales extraordinarios, como The Dark Side of the Moon (1973), Wish You Were Here (1975), Animals (1977) y The Wall (1979), discos incluidos, de hecho, entre los más influyentes en la historia de la música popular. Después de las sesiones de grabación de The Wall, el bajista Roger Waters expulsó de la banda al tecladista Rick Wright por discrepancias creativas. En 1983, el grupo lanzó el disco The Final Cut, que no alcanzó el éxito de sus obras anteriores y ni siquiera contó con gira de promoción. Argumentado que la banda estaba agotada y sin ideas, en 1985 Waters declaró la disolución de Pink Floyd. Los miembros restantes, Gilmour y Mason, se negaron a aceptar esta decisión y continuaron con el grupo, por lo cual Waters los demandó reclamando parte de los derechos de la marca Pink Floyd. Waters perdió la demanda ante los tribunales, pero, tras un acuerdo con Gilmour y Mason, obtuvo los derechos exclusivos sobre toda la imaginería que desplegaban en sus conciertos (como su famoso cerdo volador) y sobre el espectáculo audiovisual de The Wall (excepto los tres temas que había compuesto Gilmour: «Young Lust», «Run Like Hell» y «Comfortably Numb»), así como sobre todos los temas incluidos en The Final Cut; a cambio, el resto del grupo podría en lo sucesivo seguir usando sin restricciones el nombre Pink Floyd.",
+        "Tras superar estos problemas legales, Gilmour y Mason llamaron a Wright para que interveniera, como tecladista contratado, en la grabación de su siguiente trabajo, A Momentary Lapse of Reason. La relación se regularizó en el siguiente álbum, The Division Bell, con Wright ya reintegrado como miembro oficial del grupo, que recuperaba así su configuración clásica. Sin embargo, más tarde Waters se embarcó en una carrera como solista y no volvió a reunirse con sus compañeros sino hasta 24 años después; fue el 2 de julio de 2005, en Londres, en el concierto Live 8, durante el cual se pudieron escuchar las canciones «Speak to Me/Breathe», «Money», «Wish You Were Here» y «Comfortably Numb».",
+        "En noviembre de 2014 salió a la venta The Endless River, el último disco de estudio de la banda, que contiene sobre todo material grabado durante las sesiones de grabación de The Division Bell entre 1993 y 1994, y sin aportación alguna de Roger Waters.​",
+        "En una encuesta realizada por la programa británico de radio Planet Rock en 2005, Pink Floyd fue elegida como la «mejor banda de rock de todos los tiempos»."
+      ]
+    },
+    urlmusic: "PinkFloyd/singerDetail.html",
+    urlWiki: "https://es.wikipedia.org/wiki/Pink_Floyd",
+  },
+  {
+    id: "2",
+    tipo: "Grupo",
+    nombre: "Genesis",
+    imageng: "Genesis/images/GenesisFoto.jpg",
+    estilo: "Rock Sinfónico",
+    periodo: "1967-2000",
+    informacion: {
+      Fichero: "GenesisPlay.html",
+      Fotos: ["images/Concierto.jpg"],
+      Origen: "Godalming, Surrey, Reino Unido",
+      Generos: ["Rock progresivo", "Art rock", "Música elcectrónica", "Pop rock", "Soft rock", "Rock sinfónico"],
+      Actividad: ["1967-2000", "2006-2007 (gira)", "2014 (reunión)", "2020-presente"],
+      OficialWeb: "www.genesis-music.com",
+      Miembros: [
+        "Phil Collins",
+        "Tony Banks",
+        "Mike Rutherford"
+      ],
+      Descripcion: [
+        "Genesis es un grupo de rock británico creado en 1967 por Tony Banks, Mike Rutherford, Peter Gabriel y Anthony Phillips. Su último álbum de estudio, Calling All Stations, fue publicado en 1997.",
+        "Tras la partida de uno de sus principales fundadores, el guitarrista y compositor Anthony Phillips, y tras varios cambios de bateristas, el grupo quedó conformado en su primera formación 'clásica' (1970-1975) por Peter Gabriel (voz y flauta), Tony Banks (teclados), Mike Rutherford (bajo y guitarras), Phil Collins (batería y percusión) y Steve Hackett (guitarra líder).​ Tras la salida de Gabriel -en 1975-​ y de Hackett -en 1977-6 el grupo quedó reducido a un cuarteto, y posteriormente a un trío, con Collins como cantante y baterista, Rutherford como guitarrista y bajista y Banks como tecladista. A partir de la década de 1980, la música de Genesis vería un cambio fundamental de estilo, del rock progresivo hacia el pop-rock, coincidiendo con la época de mayor éxito comercial y con Phil Collins como su líder más visible.",
+        "En 1996 Phil Collins abandonó oficialmente Genesis, para centrarse en su carrera solista, y Ray Wilson fue su reemplazo como vocalista por un breve lapso de tiempo. Al no conseguir los éxitos esperados Genesis se separó definitivamente en 1998. Sin embargo en 2007 realizaron una exitosa gira de conciertos por Europa y los Estados Unidos, llamada 'Turn It On Again', tras la cual en varias ocasiones se ha comentado una posible vuelta a los estudios de grabación.​ En el 2020 se anunció su regreso a los escenarios con la gira The Last Domino?, que además a Collins, Banks y Rutherford, incluye al bajista/guitarrista y colaborador Daryl Struemer y al hijo de Phil, Nicholas Collins en la batería en reemplazo de Chester Thompson.",
+        "Con aproximadamente 155 millones de álbumes vendidos en todo el mundo, Genesis se encuentra entre los treinta grupos más exitosos en ventas de todos los tiempos. Durante su trayectoria han recibido 4 nominaciones y 1 Premio Grammy, 3 nominaciones y 1 Premio American Music y nominaciones a los Premios Brit."
+      ]
+    },
+    urlmusic: "Genesis/singerDetail.html",
+    urlwiki: "https://es.wikipedia.org/wiki/Genesis",
+  },
+  {
+    id: "3",
+    tipo: "Grupo",
+    nombre: "Supertramp",
+    imageng: "Supertramp/images/SupertrampFoto.jpg",
+    estilo: "Rock Sinfónico",
+    periodo: "1969-2005",
+    informacion: {
+      Fichero: "SupertrampPlay.html",
+      Fotos: ["images/Concierto.jpg"],
+      Origen: "Londres, Reino Unido",
+      Generos: ["Rock progresivo", "Pop progresivo", "Soft rock"],
+      Actividad: ["1968-1988", "1996-2002", "2010-2012", "2015-presente"],
+      OficialWeb: "supertramp.com",
+      Miembros: [
+        "Rick Davies",
+        "Bob Siebenberg",
+        "John Helliwell",
+        "Mark Hart",
+        "Carl Verheyen",
+        "Cliff Hugo",
+        "Lee Thornburg",
+        "Jesse Siebenberg",
+        "Gabe Dixon",
+        "Cassie Miller",
+      ],
+      Descripcion: [
+        "Supertramp es un grupo británico de rock progresivo fundado en 1969 por el músico Rick Davies. Supertramp ha sido una de las bandas más influyentes de la escena musical mundial de los setenta y ochenta del siglo XX y ha vendido más de 70 millones de discos. La primera formación de Supertramp, integrada por Davies, Roger Hodgson, Richard Palmer y Robert Millar, publicó un álbum homónimo con las características del rock progresivo de escaso éxito comercial, seguido de un segundo trabajo, Indelibly Stamped, en el que Palmer y Millar fueron sustituidos por Frank Farrell, Kevin Currie y Dave Winthrop.",
+        "Luego del escaso éxito inicial, Davies y Hodgson crearon una nueva formación, integrada por Dougie Thomson, John Helliwell y Bob Siebenberg, cuyo sonido se orientó más al pop manteniendo el estilo progresivo, con un uso predominante del piano Wurlitzer, del saxofón y de otros sonidos orquestales. El primer álbum de esta formación, Crime of the Century, favoreció el auge comercial de Supertramp, consolidado con sencillos como «Dreamer» y «Bloody Well Right». Crime of the Century fue seguido de trabajos como Crisis? What Crisis? y Even in the Quietest Moments, que labraron la reputación de Supertramp como una banda de directo.",
+        "El álbum Breakfast in America consolidó a Supertramp como una banda de éxito al alcanzar el primer puesto en la lista de los discos más vendidos de países como Alemania, Australia, Canadá, Estados Unidos, España y Francia, entre otros. Tres de sus sencillos —«Goodbye Stranger», «Take the Long Way Home» y «The Logical Song»— fueron top 20 en los Estados Unidos, donde el álbum vendió más de cuatro millones de copias. El éxito de Breakfast in America continuó con Famous Last Words, tras el cual Hodgson anunció su abandono para emprender una carrera en solitario.",
+        "Con Davies como líder de facto de Supertramp, el grupo publicó Brother Where You Bound (1985), alejándose del rock progresivo, y Free as a Bird (1987), que incorporó elementos del dance y contó con la incorporación de Mark Hart. Tras una gira posterior, Supertramp estuvo inactivo durante casi una década.",
+        "En 1996, Davies volvió a reunir al grupo, ampliado con la presencia de Carl Verheyen, Cliff Hugo, Lee Thornburg y Jesse Siebenberg, para publicar Some Things Never Change, seguido de una gira documentada en el álbum It Was the Best of Times (1999) y de Slow Motion (2002), su último álbum hasta la fecha. Tras ocho años de inactividad, Davies reformó nuevamente el grupo para realizar la gira 70-10 Tour, con motivo del 40º aniversario de la formación de Supertramp, seguida en 2015 de la gira Supertramp Forever Tour.",
+      ]
+    },
+    urlmusic: "Supertramp/singerDetail.html",
+    urlwiki: "https://es.wikipedia.org/wiki/Supertramp",
+  },
+  {
+    id: "3",
+    tipo: "Grupo",
+    nombre: "Electric Light Orchestra",
+    imageng: "Ellaor/images/EloFoto.jpg",
+    estilo: "Rock Sinfónico",
+    periodo: "1970-Presente",
+    informacion: {
+      Fichero: "ElectricPlay.html",
+      Fotos: ["images/Concierto.jpg"],
+      Origen: "Birmingham, Inglaterra, Reino Unido",
+      Generos: ["Pop rock", "Rock sinfónico", "Pop progresivo", "Art rock", "Rock progresivo", "Rock psicodélico"],
+      Actividad: ["1970-1986", "2000-2001", "2012-presente"],
+      OficialWeb: "www.jefflynneselo.com",
+      Miembros: ["Jeff Lynne - voz, bajo, guitarra, teclados, batería, chelo, productor, compositor, arreglista (1970-1986, 2000-2001, 2012-presente)", "Richard Tandy - teclados, sintetizador, bajo, coros y arreglista (1972-1986, 2000-2001, 2012-presente)"],
+      Descripcion: [
+        "Electric Light Orchestra es un grupo inglés de rock progresivo natural de Birmingham (Inglaterra) y liderado por el músico Jeff Lynne.​ La ELO se formó para dar cabida al deseo de Lynne y de Roy Wood de crear canciones de rock modernas con tintes clásicos. Después de la salida de Wood, tras el lanzamiento del álbum debut, Lynne se convirtió en el líder del grupo y compuso, arregló y produjo todas las canciones y trabajos de la formación. Entre 1971 y 1986, durante su época de mayor actividad, el grupo publicó once álbumes y obtuvo una notable popularidad con trabajos como A New World Record, Out of the Blue y Discovery.",
+        "A pesar del éxito de sus primeros sencillos en el Reino Unido, el grupo obtuvo una mayor popularidad en los Estados Unidos, donde se convirtieron en una de las bandas con mayores ventas de la industria musical. Entre 1972 y 1986, y a pesar de no obtener ningún número uno, la Electric Light Orchestra acumuló un total de quince sencillos top 10 en el Reino Unido y siete en los Estados Unidos, y mantiene el récord de éxitos en el top 40 de la historia de Billboard sin haber cosechado un número uno.​ Además, ha obtenido 38 premios de la British Phonographic Industry (BPI), veintiuno de la RIAA y diecinueve de la CRIA,​ y ha vendido más de cincuenta millones de copias a nivel mundial.",
+        "Tras su separación en 1986, Lynne volvió a reformar la Electric Light Orchestra en dos ocasiones: en 2001, con la publicación de Zoom, y en 2015, bajo el nombre de Jeff Lynne's ELO, para el lanzamiento de Alone in the Universe.",
+        "El 7 de abril de 2017, la banda entró al Salón de la Fama del Rock and Roll.",
+      ]
+    },
+    urlmusic: "Ellaor/singerDetail.html",
+    urlwiki: "https://es.wikipedia.org/wiki/Electric_Light_Orchestra",
+  },
+];
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(interpretes))
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
